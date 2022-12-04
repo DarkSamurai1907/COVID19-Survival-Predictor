58606841e71177de2ebc3de558970276f56a2c11
Aniruddh Mantrala <99598258+DarkSamurai1907@users.noreply.github.com>
2022-12-04 11:31:20 +0530

    Update The_Machine_Learning_Model.py

--- pages/The_Machine_Learning_Model.py
+++ pages/The_Machine_Learning_Model.py
@@ -24,6 +24,7 @@ st.write(fig)
 st.write("""The data was cleaned by converting continuous data to categorical, eliminating extraneous features, 
             and changing dummy values to 0s and actual values to 1.""")
 
+st.write(train.head())
          
 st.write("Utilizing MinMaxScaler, the age column was scaled and transformed into categorical data. "
          "The data was split into X and y and the model was trained.")
@@ -31,8 +32,19 @@ st.write("Utilizing MinMaxScaler, the age column was scaled and transformed into
 st.write("---")
 cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
 
+
 st.write("##### Confusion Matrix:")
 st.write(cnf_matrix)
+
+st.write("""
+         From the above Confusion Matrix we can see that
+
+         True Positive: 163037
+         False Positive: 2302
+         True Negative: 3780
+         False Negative: 862
+         """)
+
 
 st.write("---")
 st.write("""
